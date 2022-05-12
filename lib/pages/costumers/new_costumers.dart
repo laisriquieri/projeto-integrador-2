2c4e1364e10fdfcad600b7b9405adc204a66214a
laisriquieri <laisriquieri@gmail.com>
2022-05-12 11:15:54 -0300

    Refazer campos New Costumer

--- lib/pages/costumers/new_costumers.dart
+++ lib/pages/costumers/new_costumers.dart
@@ -38,114 +38,15 @@ class _NewCostumerScreenState extends State<NewCostumerScreen> {
               child: Column(
                 children: [
                   //Nome
-                  TextField(
-                    autofocus: true,
-                    decoration: InputDecoration(
-                        label: Text('Nome'),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30),
-                        ),
-                        hintText: 'José da Silva'),
-                  ),
-
-                  //CPF
-                  TextField(
-                    autofocus: true,
-                    decoration: InputDecoration(
-                        label: Text('CPF'),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30),
-                        ),
-                        hintText: '123.456.789.12'),
-                  ),
-
-                  //CEP
-                  TextField(
-                    autofocus: true,
-                    decoration: InputDecoration(
-                        label: Text('CEP'),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30),
-                        ),
-                        hintText: '00000-000'),
-                  ),
-
-                  //Logradouro
-                  TextField(
-                    autofocus: true,
-                    decoration: InputDecoration(
-                        label: Text('Logradouro'),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30),
-                        ),
-                        hintText: 'Rua dos Lírios'),
-                  ),
-
-                  //Número
-                  TextField(
-                    autofocus: true,
-                    decoration: InputDecoration(
-                        label: Text('Número'),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30),
-                        ),
-                        hintText: '123'),
-                  ),
-
-                  //Bairro
-                  TextField(
-                    autofocus: true,
-                    decoration: InputDecoration(
-                        label: Text('Bairro'),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30),
-                        ),
-                        hintText: 'Jardim da cidade'),
-                  ),
-
-                  //Cidade
-                  TextField(
-                    autofocus: true,
-                    decoration: InputDecoration(
-                        label: Text('CIdade'),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30),
-                        ),
-                        hintText: 'Franca'),
-                  ),
-
-                  //Estado - UF
-                  TextField(
-                    autofocus: true,
-                    decoration: InputDecoration(
-                        label: Text('Estado'),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30),
-                        ),
-                        hintText: 'São Paulo'),
-                  ),
-
-                  //Complemento
-                  TextField(
-                    autofocus: true,
-                    decoration: InputDecoration(
-                        label: Text('Complemento'),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30),
-                        ),
-                        hintText: 'Apartamento 1'),
-                  ),
-
-                  //Observações Gerais
-                  TextField(
-                    autofocus: true,
-                    decoration: InputDecoration(
-                        label: Text('Observações'),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30),
-                        ),
-                        hintText: 'Comprou para presentear'),
-                  ),
+                  //TextField(
+                  //  autofocus: true,
+                  //  decoration: InputDecoration(
+                  //      label: Text('Nome'),
+                  //     border: OutlineInputBorder(
+                  //      borderRadius: BorderRadius.circular(30),
+                  //  ),
+                  //  hintText: 'José da Silva'),
+                  //),
                 ],
               ),
             ),
